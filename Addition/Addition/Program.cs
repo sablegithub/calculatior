16fda8a700b81ce579b6e77d351966952f7b8b7e
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-02 07:57:32 -0700

    Addition method

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(String[] args)
+    {
+         int a = 10;
+        int b = 10;
+         int sum = a + b;
+        Console.WriteLine(sum);
+    }
+}
