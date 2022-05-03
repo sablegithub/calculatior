1c835f069b3dc1a42e1c76e6d188b5a666f35c50
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-03 06:53:30 -0700

    Calculator

---
+++ calculator/calculator/Addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Addition
+    {
+        public void add(int num1 ,int num2)
+        {
+           
+           
+            int sum = num1 + num2;
+            Console.WriteLine("Sum of Two Number is :" +sum);
+
+        }
+    }
+}
--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -1,72 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+Console.WriteLine("Enter a First Number");
+String str = Console.ReadLine();
+int num1=Convert.ToInt32(str);
+Console.WriteLine("Enter a second Number");
+String str1 = Console.ReadLine();
+int num2=Convert.ToInt32(str1);
 
-//Console.WriteLine("Hello, World!");
-
-//  static void Main(string[] args)
-// {
-//    calculator c = new calculator();
-//   c.add();
-
-//}
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Calculator
-{
-    public class Calculator
-    {
-        // Method is used for adding two numbers
-        public void Addition(int num1, int num2)
-        {
-            int sum = num1 + num2;
-            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
-        }
-
-        public void Sub(int num1, int num2)
-        {
-            int diff = 0;
-            if (num1 >= num2)
-            {
-                diff = num1 - num2;
-            }
-            else if (num1 <= num2)
-            {
-                diff = num2 - num1;
-            }
-            Console.WriteLine("Subtraction of {0} - {1} = {2}", num1, num2, diff);
-        }
-
-        public void Mul(int num1, int num2)
-        {
-            Console.WriteLine("Multiplication of {0} x {1} = {2}", num1, num2, num1 * num2);
-        }
-
-        public void Div(int num1, int num2)
-        {
-            Console.WriteLine("Division of {0} / {1} = {2}", num1, num2, num1 / num2);
-        }
-
-        // Created a Addition method
-        //public void Add()
-        //{
-        //    int num = 20;
-        //    int num2 = 40;
-        //    int sum = num + num2;
-        //    Console.WriteLine("Addition of two numbers is {0}", sum);
-        //}
-
-        //public void Addition2()
-        //{
-        //    Console.WriteLine("Enter 1st number = ");
-        //    int num1 = Convert.ToInt32(Console.ReadLine());
-
-        //    Console.WriteLine("Enter 2nd number = ");
-        //    string num = Console.ReadLine();
-        //    int num2 = Convert.ToInt32(num);
-
-        //    int sum = num1 + num2;
-        //    Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
-        //}
-    }
-}
+Addition A = new Addition();
+A.add(num1,num2);    
